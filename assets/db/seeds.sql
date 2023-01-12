f97272526134764ec6a7d27cb1334718096484dd
unknown <justinmoranre@gmail.com>
2023-01-12 14:38:25 -0800

    dependencies and names

--- assets/db/seeds.sql
+++ assets/db/seeds.sql
@@ -24,7 +24,31 @@
 
 INSERT INTO department (name)
 VALUES
-(Engineering), -- 1 --
-(Party Planning Committe), -- 2 --
-(Human Resources), -- 3 --
-(Finanace); -- 4 --
+('Engineering'), -- 1 --
+('Party Planning Committe'), -- 2 --
+('Human Resources'), -- 3 --
+('Finanace'); -- 4 --
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Lead Engineer'),
+('Junior Engineer'),
+('Engineering Manager'),
+('Advisor'),
+('Baloon Purchasor'),
+('Director'),
+('Specialist'),
+('Chief Financial Officer'),
+('Accountant');
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('James','Dean')
+('Cindy','Crawford')
+('Warren','Sapp')
+('Ryan','Reynolds')
+('Terry','Bradshaw')
+('Steve','Erkle')
+('Matt','Damon')
+('Paula','Abdul')
+('Kermit','Frog')
