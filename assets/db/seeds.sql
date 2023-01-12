0943ac9d1269fba7b3323d63046322fa65a7a261
unknown <justinmoranre@gmail.com>
2023-01-12 14:04:38 -0800

    setup db folders start

---
+++ assets/db/seeds.sql
@@ -0,0 +1,30 @@
+-- Engineering --
+    -- Lead Engineer --
+        -- James Dean --
+    -- Junior Engineer --
+        -- Cindy Crawford --
+    -- Engineering Manager --
+        -- Warren Sapp --
+-- Party Planning Committe --
+    -- Advisor --
+        -- Ryan Rynolds --
+    -- Baloon Purchasor --
+        -- Terry Bradshaw --
+-- Human Resources --
+    -- Director --
+        -- Steve Erkle --
+    -- Specialist --
+        -- Matt Damon --
+-- Finanace --
+    -- Chief Financial Officer --
+        -- Paula Abdul --
+    -- Accountant --
+        -- Kermit Frog --
+
+
+INSERT INTO department (name)
+VALUES
+(Engineering), -- 1 --
+(Party Planning Committe), -- 2 --
+(Human Resources), -- 3 --
+(Finanace); -- 4 --
